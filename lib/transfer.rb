c7c703f65bc9108b299573ddf4da14cad966899e
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-08 15:24:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   end 
   
   def execute_transaction
-   if self.valid? && sender.balance > receiver.balance
+   if self.valid? && status = "pending"
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount 
       self.status = "complete"
