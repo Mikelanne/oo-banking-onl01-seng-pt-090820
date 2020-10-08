5c7b95904f440ba3b67ec744b21cc451328098ea
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-08 15:22:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,8 @@ class Transfer
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount 
       self.status = "complete"
-    #binding.pry
+    else
+      status = "rejected"
     end 
   end 
   
