9186c14dd1de05727e23324a0367596ba21d00f7
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-08 15:28:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,7 @@ class Transfer
       receiver.balance = receiver.balance + amount 
       self.status = "complete"
     else
+      binding.pry
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end 
