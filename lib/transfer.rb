43c042bae37f5d273b0741bd7a69ad252f403848
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-08 15:28:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,13 +20,13 @@ class Transfer
   end 
   
   def execute_transaction
-   if self.valid? && sender.balance > @amount && status == "pending"
+   if self.valid? && status == "pending"
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount 
       self.status = "complete"
     else
-      status = "rejected"
       "Transaction rejected. Please check your account balance."
+      self.status = "rejected"
     end 
   end 
   
